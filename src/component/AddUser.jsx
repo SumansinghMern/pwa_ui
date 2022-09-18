463d97595220a4e6f4b504dac8db84d60262d932
suman <suman@active.agency>
2022-09-19 02:09:49 +0530

    first commit

---
+++ src/component/AddUser.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react"
+
+const AddUser = (props) => {
+
+    const [name, changeName] = useState('')
+    const [email, changeEmail] = useState('')
+    const [profile, changeProfile] = useState({})
+    const [phone, changePhone] = useState('')
+
+
+    const changeInput = (value, name) => {
+        if (name === 'name') {
+            changeName(value)
+        }
+        if (name === 'email') {
+            changeEmail(value)
+        }
+        if (name === 'profile') {
+            changeProfile(value[0])
+        }
+        if (name = 'phone') {
+            changePhone(value)
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('name',name);
+        formData.append('email',email);
+        formData.append('phone',phone)
+        formData.append('profile',profile)
+
+        fetch('http://localhost:5000/addUser',{
+            method:'POST',
+            headers: {
+                // 'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Type': 'multipart/form-data',
+                'Boundary':'MyBoundary'
+            },
+            data:formData
+        })
+    }
+
+    return (
+        <form 
+            class="product-form" 
+            onSubmit={(e) => onSubmit(e)}
+            >
+            <div class="form-control">
+                <label for="title">Name</label>
+                <input type="text" name="name" onChange={(e) => changeInput(e.target.value, 'name')} id="title" value={name} />
+            </div>
+            <div class="form-control">
+                <label for="imageUrl">Email</label>
+                <input type="text" name="email" onChange={(e) => changeInput(e.target.value, 'email')} id="imageUrl" value={email} />
+            </div>
+            <div class="form-control">
+                <label for="image">Image</label>
+                <input
+                    type="file"
+                    name="image"
+                    onChange={(e) => changeInput(e.target.files, 'profile')}
+                    id="image"
+                />
+            </div>
+            <div class="form-control">
+                <label for="price">Phone</label>
+                <input type="number" name="phone" onChange={(e) => changeInput(e.target.value, 'phone')} id="price" step="0.01" value={phone} />
+            </div>
+
+            <button class="btn" type="submit">Submit</button>
+
+        </form>
+    )
+}
+
+export default AddUser
